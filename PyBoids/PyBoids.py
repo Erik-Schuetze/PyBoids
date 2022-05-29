93baf151ba6e5836dbb2aa2c7070d4a1138e403d
Erik Schuetze <erik.schuetze99@gmail.com>
2022-05-29 23:10:04 +0200

    Projektdateien hinzufügen.

---
+++ PyBoids/PyBoids.py
@@ -0,0 +1,10 @@
+import sys
+import random
+
+from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QPainter, QColor, QFont
+
+from pynput import keyboard
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
